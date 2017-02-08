c3ff821003fa0604ab42ce3f3b10f16333203d63
nzoLogic <nzoLogic@gmail.com>
2017-02-08 15:52:15 -0800

    maps added in JS

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,5 +1,6 @@
 //= require jquery
 //= require jquery_ujs
+//= require map
 //= require materialize-sprockets
 //= require materialize
 //= require_tree .
---
+++ app/assets/javascripts/map.js
@@ -0,0 +1,11 @@
+console.log('hey');
+
+$(document).ready(function(){
+  function initMap() {
+          map = new google.maps.Map(document.getElementById('map'), {
+            center: {lat: -34.397, lng: 150.644},
+            zoom: 8
+          });
+        }
+        initMap()
+})
