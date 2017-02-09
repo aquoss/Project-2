5e30dce6ac338364b8c6b381cd7489082e8e0161
ryanthomas92 <rthoma11@villanova.edu>
2017-02-09 12:03:00 -0800

    added authorization to session helper and controller functions

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
-  has_secure_password
-  
   has_many :hiders, class_name: "Game"
   has_many :seekers, class_name: "Game"
+
+  has_secure_password
+
+  def self.confirm(params)
+    @user = User.find_by({email: params[:email]})
+    @user ? @user.authenticate(params[:password]) : false
+  end
 end
