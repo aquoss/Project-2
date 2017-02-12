3fefd4b64942f9fe53cfdd121741c922ac9a4f9d
aquoss <amberquoss@gmail.com>
2017-02-11 18:56:25 -0800

    scrollfire and parallax divs working

--- app/assets/javascripts/materialize.js
+++ app/assets/javascripts/materialize.js
@@ -2,4 +2,14 @@ $(document).ready(function(){
   $('.parallax').parallax();
   $('.dropdown-button').dropdown();
   $('.collapsible').collapsible();
+
+  var options = [
+    {selector: '.rule-one', offset: 200, callback: textFadeIn}
+  ];
+  Materialize.scrollFire(options);
+
 })
+
+function textFadeIn(element){
+  Materialize.showStaggeredList($(element));
+}
